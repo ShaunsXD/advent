de2ed8664c8405d75414f718eeaac76439d13a6e
EX <exu39@gatech.edu>
2024-12-06 10:43:56 -0600

    UGLY BRUTE FORCE SOLUTION LOL part 2 done

--- src/day6/day6.py
+++ src/day6/day6.py
@@ -59,8 +59,100 @@ def part1():
     print(countX(arr))
 
 
+def findCycle(arr):
+    r, c = findGuard(arr)
+    dir = arr[r][c]
+    count = 0
+    while 0 <= r < len(arr) and 0 <= c < len(arr[0]):
+        count += 1
+        if count >= 10000:
+            return True
+
+        arr[r][c] = "X"
+
+        if dir == "^":
+            arr[r][c] = "|"
+        elif dir == ">":
+            arr[r][c] = "-"
+        elif dir == "v":
+            arr[r][c] = "d"
+        elif dir == "<":
+            arr[r][c] = "l"
+
+        if dir == "^":
+            r -= 1
+        elif dir == "v":
+            r += 1
+        elif dir == "<":
+            c -= 1
+        elif dir == ">":
+            c += 1
+
+        if r < 0 or r >= len(arr) or c < 0 or c >= len(arr[0]):
+            break
+
+        char = arr[r][c]
+        if char == "|" and dir == "^":
+            for l in arr:
+                print("".join(l))
+            return True
+        if char == "-" and dir == ">":
+            for l in arr:
+                print("".join(l))
+            return True
+        if char == "d" and dir == "v":
+            for l in arr:
+                print("".join(l))
+            return True
+        if char == "l" and dir == "<":
+            for l in arr:
+                print("".join(l))
+            return True
+
+        if char == "#":
+            if dir == "^":
+                r += 1
+                dir = ">"
+            elif dir == "v":
+                r -= 1
+                dir = "<"
+            elif dir == "<":
+                c += 1
+                dir = "^"
+            elif dir == ">":
+                c -= 1
+                dir = "v"
+    return False
+
+def part2():
+    arr = []
+    cycles = 0
+    with open("6.txt", "r") as file:
+        for l in file:
+            line = []
+            l = l.strip()
+            for char in l:
+                line.append(char)
+            arr.append(line)
+    i = 0
+    for r in range(len(arr)):
+        for c in range(len(arr[0])):
+            new_arr = [l[:] for l in arr]
+            if new_arr[r][c] == ".":
+                new_arr[r][c] = "#"
+                i += 1
+                #print(i)
+                if findCycle(new_arr):
+                    cycles += 1
+    print(cycles)
+
+
+
+
+
+
 if __name__ == "__main__":
     part1()
-
+    part2()
 
 
