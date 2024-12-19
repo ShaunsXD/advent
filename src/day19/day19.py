c032ec4cef73d079b8b48106813389781fc78bdd
EX <exu39@gatech.edu>
2024-12-19 09:32:23 -0600

    easy stuff

---
+++ src/day19/day19.py
@@ -0,0 +1,28 @@
+import functools
+
+
+patterns = []
+strings = []
+with open("19.txt", "r") as file:
+    data = file.read().split("\n\n")
+    patterns = data[0].split(", ")
+    strings = data[1].split("\n")
+
+#Count all possible ways you can make the string with patterns
+@functools.cache
+def patternMatch(string):
+    if not string:
+        return 1
+    count = 0
+    for pattern in patterns:
+        if string.startswith(pattern):
+            count += patternMatch(string[len(pattern):])
+    return count
+
+possible = 0
+count = 0
+for string in strings:
+    if patternMatch(string):
+        possible += 1
+    count += patternMatch(string)
+print(possible, count)
